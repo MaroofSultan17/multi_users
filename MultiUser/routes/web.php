bd39c74d7f75338233213c5e878f5bfb4286fa7f
MaroofSultan17 <maroofsultan17@gmail.com>
2024-05-30 01:52:28 +0500

    upload project

---
+++ MultiUser/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Admin
+Route::middleware(['auth', 'user-access:admin'])->group(function () {
+    Route::get('/admin/dashboard', [HomeController::class, 'admin'])->name('admin.dashboard');
+});
+
+//Teacher
+Route::middleware(['auth', 'user-access:teacher'])->group(function () {
+    Route::get('/teacher/dashboard', [HomeController::class, 'teacher'])->name('teacher.dashboard');
+});
+
+//Student
+Route::middleware(['auth', 'user-access:student'])->group(function () {
+    Route::get('/student/dashboard', [HomeController::class, 'student'])->name('student.dashboard');
+});
